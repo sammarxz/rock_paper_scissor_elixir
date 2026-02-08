d8dafa312c1864545d9691a46a0387621f47057c
Sam Marxz <sammarxzz@gmail.com>
2026-02-08 12:57:55 -0300

    mix new

---
+++ lib/rock_paper_scissor.ex
@@ -0,0 +1,5 @@
+defmodule RockPaperScissor do
+  def hello do
+    :world
+  end
+end
---
+++ test/rock_paper_scissor_test.exs
@@ -0,0 +1,8 @@
+defmodule RockPaperScissorTest do
+  use ExUnit.Case
+  doctest RockPaperScissor
+
+  test "greets the world" do
+    assert RockPaperScissor.hello() == :world
+  end
+end
