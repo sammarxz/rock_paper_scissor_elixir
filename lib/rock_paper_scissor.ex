94a685964ac18b5d6f5f284eaea0783bc1285195
Sam Marxz <sammarxzz@gmail.com>
2026-02-09 10:05:08 -0300

    refactor to add more "elixir way" of programming

--- lib/rock_paper_scissor.ex
+++ lib/rock_paper_scissor.ex
@@ -3,12 +3,13 @@ defmodule RockPaperScissor do
 
   @available_hands [:rock, :paper, :scissor]
 
-  def play() do
+  def play do
     welcome_message()
 
     get_user_response()
     |> handle_user_response()
     |> match_hands()
+    |> print_result()
   end
 
   defp welcome_message do
@@ -21,33 +22,30 @@ defmodule RockPaperScissor do
     |> String.trim()
   end
 
-  defp handle_user_response(response) do
-    case response do
-      "rock" -> :rock
-      "paper" -> :paper
-      "scissor" -> :scissor
-      _ -> :error
-    end
-  end
+  defp handle_user_response("rock"), do: {:ok, :rock}
+  defp handle_user_response("paper"), do: {:ok, :paper}
+  defp handle_user_response("scissor"), do: {:ok, :scissor}
+  defp handle_user_response(_), do: {:error, :invalid_choice}
 
-  defp match_hands(:error), do: IO.puts("Please, select only: rock, paper or scissor")
+  defp match_hands({:error, _}), do: {:error, "Please, select only: rock, paper or scissor"}
 
-  defp match_hands(player) do
+  defp match_hands({:ok, player}) do
     player_hand = Hand.build(player)
     computer_hand = Hand.build(Enum.random(@available_hands))
 
     verify_result(player_hand, computer_hand)
   end
 
-  defp verify_result(%{name: same}, %{name: same}) do
-    IO.puts("Draw! Both chose #{same}")
-  end
+  defp verify_result(%{name: same}, %{name: same}), do: {:draw, same}
 
   defp verify_result(player, computer) do
-    if player.name in computer.lose do
-      IO.puts("You win! #{player.name} beats #{computer.name}")
-    else
-      IO.puts("You lose! #{computer.name} beats #{player.name}")
-    end
+    if player.name in computer.lose,
+      do: {:win, player.name, computer.name},
+      else: {:lose, player.name, computer.name}
   end
+
+  defp print_result({:error, message}), do: IO.puts(message)
+  defp print_result({:draw, hand}), do: IO.puts("Draw! Both chose #{hand}")
+  defp print_result({:win, p, c}), do: IO.puts("You win! #{p} beats #{c}")
+  defp print_result({:lose, p, c}), do: IO.puts("You lose! #{c} beats #{p}")
 end
